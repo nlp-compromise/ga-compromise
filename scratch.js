3df630697949692789df4211ee280877e66e587e
spencer kelly <spencerkelly86@gmail.com>
2026-02-18 11:22:34 -0500

    add instructions

--- scratch.js
+++ scratch.js
@@ -1,5 +1,14 @@
 import nlp from './src/index.js'
 nlp.verbose('tagger')
 
+// to run:
+// npm install
 
-let doc = nlp('shiúil an buachaill go dtí an siopa').debug()
+// then run:
+// node scratch.js
+
+// change something in /data
+// then run: npm run pack
+
+let doc = nlp('shiúil an buachaill go dtí an siopa')
+doc.debug()
